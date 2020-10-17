0d1cdfe6ffbd6ae3a1534ba86356b300f25417e7
Eduardo Pinho <enet4mikeenet@gmail.com>
2020-10-17 17:29:48 +0100

    New iteration
    - update rust toolchain to nightly-2020-10-17
    - update target specification
    - use ld directly, instead of gcc
    - gitignore exe's at root
    - add coff-to-djgpp.pl
    - clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,9 @@ use libc::{c_char, c_int};
 
 extern "C" {
     pub fn printf(format: *const c_char, ...) -> c_int;
-    pub fn cputs(s: *const c_char) -> c_int;
     pub fn puts(s: *const c_char) -> c_int;
-    pub fn clrscr() -> ();
-}
-
-extern "C" {
     pub fn exit(c: c_int);
 }
-/*
-#[start]
-#[no_mangle]
-pub extern "C" fn main(_argc: isize, _argv: *const *const c_char) -> c_int {
-    unsafe {
-        for _ in 0..10 {
-            cputs(b"Rust says hello DOS!\0".as_ptr() as *const c_char);
-    
-            let mut dummy = 0;
-            for _ in 0..50_000 {
-                // some arbitrary memory writes to kill time
-                core::ptr::write_volatile(&mut dummy, 5);
-            }
-            clrscr();
-            for _ in 0..50_000 {
-                // some arbitrary memory writes to kill time
-                core::ptr::write_volatile(&mut dummy, 5);
-            }
-        }
-    }
-    0
-}
-*/
 
 #[start]
 #[no_mangle]
