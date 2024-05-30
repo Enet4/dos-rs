daae6402c880dbf164c1b61ac5d11c8d93396296
Eduardo Pinho <enet4mikeenet@gmail.com>
2024-05-30 15:56:21 +0100

    Add opl example
    - very rudimentary VGM file player

--- examples/opl/src/lib.rs
+++ examples/opl/src/lib.rs
@@ -4,20 +4,21 @@
 #![no_main]
 extern crate alloc;
 use alloc::format;
-use dos_x::djgpp::{dos::delay, stdio::puts, stdlib::exit};
-use dos_x::adlib::{self, detect_adlib, reset_adlib};
-use dos_x::sb::detect_sb;
 use core::ffi::CStr;
+use core::hint::unreachable_unchecked;
 use core::panic::PanicInfo;
-
-use dos_x::key::wait_for_keypress;
+use dos_x::adlib::{self, detect_adlib, reset_adlib};
+use dos_x::djgpp::{dos::delay, stdio::puts, stdlib::exit};
+use dos_x::key::get_keypress;
+use dos_x::println;
+use dos_x::sb::detect_sb;
+use opbinary::vgm::OplCommand;
 
 #[allow(non_camel_case_types)]
 type c_char = i8;
 
 #[no_mangle]
 fn dos_main() {
-
     // try to read command line arguments
     let args = dos_x::argv();
 
@@ -30,14 +31,13 @@ fn dos_main() {
                 let msg = format!(" - {}\0", str.to_string_lossy());
                 puts(msg.as_ptr() as *const c_char);
             }
-            Some(CStr::from_ptr(args[0]))
+            Some(CStr::from_ptr(args[1]))
         }
     } else {
         None
     };
 
     unsafe {
-
         match detect_adlib() {
             0 => {
                 puts(b"No Adlib sound card detected :(\0".as_ptr() as *const c_char);
@@ -56,31 +56,26 @@ fn dos_main() {
                 core::hint::unreachable_unchecked()
             }
             Some((addr, irq, dma)) => {
-                let msg = format!("Sound Blaster sound card detected!\n addr: 0x{:x}\n irq: {}\n dma: {}\0", addr, irq, dma);
+                let msg = format!(
+                    "Sound Blaster sound card detected!\n addr: 0x{:x}\n irq: {}\n dma: {}\0",
+                    addr, irq, dma
+                );
                 puts(msg.as_ptr() as *const _);
             }
         };
 
-        puts(b"Press Enter to continue\0".as_ptr() as *const c_char);
+        busy_wait(1_000);
 
-        busy_wait(5_000);
-        wait_for_keypress(0x1c);
+        match vgm_filename {
+            Some(filename) => run_player(filename),
+            None => run_note_loop(),
+        }
 
-        app_loop();
-
-        puts(b"< Bye >
- -----
-    \\
-     \\
-       _~^~^~_
-   \\) /  o o  \\ (/
-     '_   -   _'
-     / '-----' \\\0".as_ptr() as *const c_char);
+        println!("Bye");
     }
 }
 
-fn app_loop() {
-
+fn run_note_loop() {
     // initialize
     reset_adlib();
 
@@ -104,14 +99,11 @@ fn app_loop() {
         adlib::write_command(0x83, 0x77);
     }
 
-    unsafe {
-        puts(b"Now playing...\0".as_ptr() as *const c_char);
-    }
+    println!("Now playing...");
 
     let mut shutdown = false;
     let mut k = 0;
     while !shutdown {
-
         // play a note
         // (frequency depends on k)
         let freq: u16 = match k {
@@ -126,11 +118,7 @@ fn app_loop() {
             _ => 0x2AE,
         };
         let [freq_lo, freq_hi] = freq.to_le_bytes();
-        let octave: u8 = if k == 0 {
-            3
-        } else {
-            4
-        };
+        let octave: u8 = if k == 0 { 3 } else { 4 };
         unsafe {
             // set voice frequency LSB
             adlib::write_command(0xa0, freq_lo);
@@ -161,6 +149,135 @@ fn app_loop() {
     // turn off voice
     unsafe {
         adlib::write_command(0xb0, 0x00);
+    }
+    reset_adlib();
+}
+
+fn run_player(filename: &CStr) {
+    // initialize
+    reset_adlib();
+
+    println!("Opening file {}...", filename.to_string_lossy());
+
+    let file_data = dos_x::fs::read(filename).unwrap_or_else(|e| {
+        println!("Failed to read file: {}", e);
+        unsafe {
+            exit(-2);
+            unreachable_unchecked();
+        }
+    });
+
+    println!("{} bytes fetched successfully", file_data.len());
+    let mut stop = false;
+    while !stop {
+        unsafe {
+            delay(100);
+        }
+
+        if dos_x::key::get_keypress() == 0x1c {
+            stop = true;
+        }
+    }
+
+    let vgm = match opbinary::vgm::Vgm::from_bytes(&file_data) {
+        Ok(vgm) => {
+            println!(
+                "VGM v{:x}, {} samples",
+                vgm.header.base_header.version, vgm.header.total_samples
+            );
+
+            if vgm.header.ym3812_clock == 0 && vgm.header.ymf262_clock == 0 {
+                println!("Not an OPL music file");
+                unsafe {
+                    exit(-3);
+                    unreachable_unchecked();
+                }
+            }
+            if vgm.header.ymf262_clock != 0 {
+                println!("YMF262 clock: {} Hz (OPL3)", vgm.header.ymf262_clock);
+            }
+            if vgm.header.ym3812_clock != 0 {
+                println!("YM3812 clock: {} Hz (OPL2)", vgm.header.ym3812_clock);
+            }
+
+            vgm.into_opl_vgm()
+        }
+        Err(e) => {
+            println!("Could not read VGM file: {}", e);
+            unsafe {
+                exit(-3);
+                unreachable_unchecked();
+            }
+        }
+    };
+
+    println!("Now playing... (Hold Esc to stop)");
+
+    fn samples_to_ms(samples: u32) -> u32 {
+        samples * 10 / 441
+    }
+
+    for cmd in vgm.opl_commands {
+        match cmd {
+            OplCommand::Opl3 {
+                port: 0,
+                address,
+                data,
+            } => unsafe {
+                adlib::write_command_l(address, data);
+            },
+            OplCommand::Opl3 {
+                port: 1,
+                address,
+                data,
+            } => unsafe {
+                adlib::write_command_r(address, data);
+            },
+            OplCommand::Opl2 { address, data }
+            | OplCommand::Opl3 {
+                port: _,
+                address,
+                data,
+            } => unsafe {
+                adlib::write_command(address, data);
+            },
+            OplCommand::Wait { samples } => unsafe {
+                delay(samples_to_ms(samples as u32));
+                if get_keypress() == 27 {
+                    break;
+                }
+            },
+            OplCommand::SmallWait { n } => unsafe {
+                delay(samples_to_ms(n as u32 + 1));
+                if get_keypress() == 27 {
+                    break;
+                }
+            },
+            OplCommand::Wait735 => unsafe {
+                delay(samples_to_ms(735));
+                if get_keypress() == 27 {
+                    break;
+                }
+            },
+            OplCommand::Wait882 => unsafe {
+                delay(samples_to_ms(882));
+                if get_keypress() == 27 {
+                    break;
+                }
+            },
+        }
+    }
+
+    // turn off voice for every channel
+    unsafe {
+        for i in 0..9 {
+            adlib::write_command(0xb0 | i, 0x00);
+            delay(1);
+            adlib::write_command_l(0xb0 | i, 0x00);
+            delay(1);
+            adlib::write_command_r(0xb0 | i, 0x00);
+            delay(1);
+        }
     }
     reset_adlib();
 }
