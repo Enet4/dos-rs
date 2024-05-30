6a70f936a1b0a707a726efe6c7eb36b0ecfc918c
Eduardo Pinho <enet4mikeenet@gmail.com>
2024-05-30 16:09:47 +0100

    Update bundle and bundler

--- bundle.sh
+++ bundle.sh
@@ -2,15 +2,23 @@
 set -eu
 
 # only call build.sh if the file was not built yet
-if [ ! -f ./build/release/dos_rs.exe ]; then
+if [ ! -f ./build/release/ferris.exe ]; then
     ./build.sh release
 fi
-# add dos_rs.exe, dosbox.conf and CWSDPMI.EXE into new zip file
+if [ ! -f ./build/release/opl.exe ]; then
+    ./build-opl.sh release
+fi
+# add ferris.exe, assets, dosbox.conf and CWSDPMI.EXE into new zip file
 cd bundle
-cp ../build/release/dos_rs.exe ./dos_rs.exe
 rm -f dos_rs.zip
-zip -q dos_rs.zip dos_rs.exe .jsdos/dosbox.conf CWSDPMI.EXE
-rm dos_rs.exe
+cp ../build/release/ferris.exe ../build/release/opl.exe ./
+zip -q dos_rs.zip \
+    CWSDPMI.EXE \
+    ferris.exe \
+    opl.exe \
+    abnormal.vgm \
+    .jsdos/dosbox.conf
+rm ferris.exe opl.exe
 
 # move it as dos_rs.jsdos
 cp dos_rs.zip ../dos_rs.jsdos
